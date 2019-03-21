4a8bce67402f5de9ba9fd158a1902e122e55b4bc
Collin Woodruff <Cwoodruff@mail.greenriver.edu>
2019-03-21 17:21:07 -0400

    Everything one more time

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'vendor/autoload.php';
+
+session_start();
+
+//Connect to DB
+require 'model/dbConnection.php';
+$dbh = connect();
+
+if(!$dbh) //don't go any further if we can't connect to the database
+{
+    exit;
+}
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function ($f3)
+{
+    $students = getAllStudents();
+    $f3->set('students', $students);
+    echo Template::instance()->render("views/all-students.html");
+});
+
+$f3->route('GET|POST /add', function ($f3)
+{
+    if(isset($_POST['submit']))
+    {
+        $sid = $_POST['sid'];
+        $last = $_POST['last'];
+        $first = $_POST['first'];
+        $birthdate = $_POST['birthdate'];
+        $gpa = $_POST['gpa'];
+        $advisor = $_POST['advisor'];
+        $success = addStudent($sid, $last, $first, $birthdate, $gpa, $advisor);
+        if($success)
+        {
+            //create a student object
+            $student = new Student($sid, $last, $first, $birthdate, $gpa, $advisor);
+            //add to session
+            $_SESSION['student'] = $student;
+            //reroute
+            $f3->reroute('/');
+        }
+    }
+    echo Template::instance()->render("views/add.html");
+});
+
+$f3->route('GET /view/@sid', function($f3, $params) {
+    $sid = $params['sid'];
+    $student = getStudent($sid);
+    $f3->set('student', $student);
+    $template = new Template();
+    echo $template->render('views/view-student.html');
+});
+
+//Run fat free
+$f3->run();
---
+++ model/dbConnection.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Collin Woodruff
+ * Date: 3/21/2019
+ * Time: 1:16 PM
+ */
+//require our db config file
+require_once('/home/cwoodruf/config.php');
+function connect()
+{
+    try {
+        //Instantiate a db object
+        $dbh = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
+        return $dbh;
+    }
+    catch(PDOException $ex)
+    {
+        echo $ex->getMessage();
+        return false;
+    }
+}
+function getAllStudents()
+{
+    global $dbh;
+    $sql = "SELECT * FROM student ORDER BY last, first";
+    $statement = $dbh->prepare($sql);
+    $statement->execute();
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+function addStudent($sid, $last, $first, $birthdate, $gpa, $advisor)
+{
+    global $dbh;
+    $sql = "INSERT INTO student VALUES(:sid, :last, :first, :birthdate, :gpa, :advisor)";
+    $statement = $dbh->prepare($sql);
+    //bind parameters
+    $statement->bindParam(':sid', $sid, PDO::PARAM_STR);
+    $statement->bindParam(':last', $last, PDO::PARAM_STR);
+    $statement->bindParam(':first', $first, PDO::PARAM_STR);
+    $statement->bindParam(':birthdate', $birthdate, PDO::PARAM_STR);
+    $statement->bindParam(':gpa', $gpa, PDO::PARAM_STR);
+    $statement->bindParam(':advisor', $advisor, PDO::PARAM_STR);
+    //execute the statement and return true or false if it was successful
+    return $statement->execute();
+}
+function getStudent($sid) {
+    global $dbh;
+    $sql = "SELECT * FROM student WHERE sid = '$sid'";
+    $statement = $dbh->prepare($sql);
+    $statement->execute();
+    $result = $statement->fetch(PDO::FETCH_ASSOC);
+    return $result;
+}
